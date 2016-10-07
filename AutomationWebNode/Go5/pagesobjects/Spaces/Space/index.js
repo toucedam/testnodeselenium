13efd1136b45653dc3e62882dbda7507f3806930
marinatouceda <marinatouceda@gointegro.com>
2016-10-07 17:32:24 -0300

    Adding Space page

--- AutomationWebNode/Go5/pagesobjects/Spaces/Space/index.js
+++ AutomationWebNode/Go5/pagesobjects/Spaces/Space/index.js
@@ -6,32 +6,18 @@ class Space extends BasePage {
 		super(webdriver);
 	}
 
-	isLoaded () {
-		return super.waitForDisplayed(goButtonLogin, 50000);
+	goToLeftSpace () {
+
+		 this.goTo(Selectors.openMenu)
+		 then(() => {
+				this.goTo(Selectors.leaveSpace)
+				});
 	}
 
- 	sendUsername (username) {
- 		this.driver.findElement (By.css(inputEmail)).sendKeys(username);
- 	}
-
- 	sendPassword (password) {
-		this.driver.findElement(By.css(inputPassword)).sendKeys(password);
- 	}
-
- 	pressLoginButton () {
- 		this.driver.findElement(By.css(goButtonLogin)).click();
- 	}
-
- 	login (username,password) {
-
- 		this.sendUsername(username);
- 		this.sendPassword(password);
- 		this.pressLoginButton();
- 		return this.driver.wait(until.elementLocated(By.css(home)),50000);
- 	}
-
-}
-
-export default Space;
+	goToMySpaces(){
+		return this.goTo(Selectors.goToMySpaces);
 	}
 }
+
+export default Spaces;
+
--- AutomationWebNode/Go5/pagesobjects/Spaces/Space/selectors.js
+++ AutomationWebNode/Go5/pagesobjects/Spaces/Space/selectors.js
@@ -1,17 +1,10 @@
 	export default const selectors = {
 
-	goToRecommended: '.igotitleswhite',
-	goToMySpaces: '.igopasswordwhite',
+		social: 'igosocialswhite',
+		members: '.open menu li:nth-child(2) .igouserwhite ',
+		openMenu: 'horizontalmenu',
+		leaveSpace: '.last .dotmenu li:first-child',
 
-	// Order
-	orderSpaceName:'.igocaretdown',
-
-	//Spaces
-	editfourthRow:'.tables tbody tr:nth-child(4) td:nth-child(5) a',
-	firstType: '.tables  tr:nth-child(1) td:nth-child(3)',
-	firstSpace:'.tables tbody tr:nth-child(1) td:nth-child(2) a',
-	firstName: '.tables tbody tr:nth-child(1) td:nth-child(2) a',
-	secondName: '.tables tbody tr:nth-child(2) td:nth-child(2) a',
 
 	}
 
